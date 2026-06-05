09c16ea651b080fc717095b8d66d12d39010d483
kammi <114113059+k4mmi@users.noreply.github.com>
2026-06-05 20:27:37 +0200

    Add files via upload

--- src/sections/DownloadSection.jsx
+++ src/sections/DownloadSection.jsx
@@ -4,10 +4,10 @@ import "./DownloadSection.scss"
 export function DownloadSection() {
     return (
         <Section className="downloadSection">
-            <a href="/" className="downloadSection__download">Download</a>
+            <a href="https://github.com/aoifemooney/makingbiorhyme/tree/b3c0488559ad7c42e11b71e65d255344faff63b9/fonts" className="downloadSection__download">Download</a>
             <div className="downloadSection__info">
                 <span className="downloadSection__links"><a href="https://github.com/aoifemooney/makingbiorhyme">Github</a>/<a href="https://fonts.google.com/specimen/BioRhyme">Google Fonts</a></span>
-                <a href="https://github.com/aoifemooney/makingbiorhyme/tree/b3c0488559ad7c42e11b71e65d255344faff63b9/fonts" className="downloadSection__license">Open SIL font license</a>
+                <a href="https://raw.githubusercontent.com/aoifemooney/makingbiorhyme/refs/heads/gh-pages/OFL.txt" className="downloadSection__license">Open SIL font license</a>
             </div>
         </Section>
     );
